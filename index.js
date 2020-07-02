5c4354c2890ae0adebd8506c00d26390b7339684
Perke1 <springerjan2@gmail.com>
2020-07-02 23:43:28 +0200

    Some Progress

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const fetch = require("node-fetch")
 
+// Fetching item from json
+
 fetch("https://www.supremenewyork.com/mobile_stock.json")
   .then(res => res.json())   
      .then((out) => {
-     const newCategory = out.products_and_categories.new
+  const newCategory = out.products_and_categories.new
+      // Defining variable as our desirable product
   const desiredItem = newCategory.find((item) => item.name.includes("Motion"))
-
+      // Defining variable with id of our product
   const itemId = desiredItem.id          
-
-
+      // Fetching more info about the product
 fetch(`https://www.supremenewyork.com/shop/${itemId}.json`)
-
 .then(res => res.json())
 .then((out) => {
   const styles = out.styles
   const desiredStyleId = styles.find((style) => style.name.includes("Black"))
-  console.log(desiredStyleId.id)
-  const sizes = out.styles.sizes
-  const desiredSize = sizes.find((size) => size.includes("Small"))
-  console.log(desiredSize)
+  console.log(desiredStyleId.id) // storing colour id
+  const sizes = styles[0].sizes
+  const test = sizes.find((size) => size.name.includes("XLarge")) 
+  const desiredSize = test
+  console.log(desiredSize.id) // storing size id
 }).catch(err => console.error(err))
 }).catch(err => console.error(err))
