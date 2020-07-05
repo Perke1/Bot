429da6fc6b84a5ddcb0f59aa7722539121d9b3b0
Perke1 <springerjan2@gmail.com>
2020-07-05 16:53:27 +0200

    Trying adding to cart with post request

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const fetch = require("node-fetch")
+const axios = require('axios');
 
 // Fetching item from json
 
 fetch("https://www.supremenewyork.com/mobile_stock.json")
   .then(res => res.json())   
      .then((out) => {
-  const newCategory = out.products_and_categories.new
+  const newCategory = out.products_and_categories.Accessories
       // Defining variable as our desirable product
-  const desiredItem = newCategory.find((item) => item.name.includes("Motion"))
+  const desiredItem = newCategory.find((item) => item.name.includes("Supreme®/Hanes® Boxer Briefs (4 Pack)"))
       // Defining variable with id of our product
   const itemId = desiredItem.id          
       // Fetching more info about the product
@@ -16,10 +17,31 @@ fetch(`https://www.supremenewyork.com/shop/${itemId}.json`)
 .then((out) => {
   const styles = out.styles
   const desiredStyleId = styles.find((style) => style.name.includes("Black"))
-  console.log(desiredStyleId.id) // storing colour id
+ // console.log(desiredStyleId.id) // storing colour id
   const sizes = styles[0].sizes
   const test = sizes.find((size) => size.name.includes("XLarge")) 
   const desiredSize = test
-  console.log(desiredSize.id) // storing size id
+  //console.log(desiredSize.id) // storing size id
+
+  console.log("Trying add to cart")
+
+  const config = {
+    headers: {'content-type': 'application/x-www-form-urlencoded',
+    'User-Agent': 'Mozilla/5.0 (iPhone9,3; U; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A403 Safari/602.1'
+  }}
+
+  axios.post(`https://supremenewyork.com/shop/${itemId}/add.json`, 
+  {
+    style: desiredStyleId.id,
+    size: desiredSize.id,
+    qty: 1
+  }, config)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
 }).catch(err => console.error(err))
 }).catch(err => console.error(err))
